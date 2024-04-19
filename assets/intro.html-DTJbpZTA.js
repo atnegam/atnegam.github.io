37f9e492a261ef60bce20c35d539203aeed663d1
younhone <1497259719@qq.com>
2024-04-19 20:04:07 +0800

    deploy

---
+++ assets/intro.html-DTJbpZTA.js
@@ -0,0 +1,11 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as t,c as n,a}from"./app-KrCVGb1t.js";const i={},o=a(`<h1 id="about-me" tabindex="-1"><a class="header-anchor" href="#about-me"><span>About me</span></a></h1><div class="language-C++ line-numbers-mode" data-ext="C++" data-title="C++"><pre class="language-C++"><code>西南科技大学 · 计算机科学与技术硕士
+
+12/2019 - 03/2020 autowise.ai
+测试与运维组 · 测试
+
+04/2023 - 08/2023 XFUSION
+操作系统开发部 · 操作系统研发
+
+03/2024 - Present 四川信息职业技术学院
+网络与通信学院 · 助教
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),r=[o];function s(c,l){return t(),n("div",null,r)}const p=e(i,[["render",s],["__file","intro.html.vue"]]),u=JSON.parse('{"path":"/intro.html","title":"About me","lang":"zh-CN","frontmatter":{"icon":"circle-info","cover":false,"article":false,"description":"About me","head":[["meta",{"property":"og:url","content":"https://mister-hope.github.io/intro.html"}],["meta",{"property":"og:title","content":"About me"}],["meta",{"property":"og:description","content":"About me"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"article:author","content":"magenta"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"About me\\",\\"description\\":\\"About me\\"}"]]},"headers":[],"git":{},"readingTime":{"minutes":0.25,"words":74},"filePathRelative":"intro.md","excerpt":"\\n<div class=\\"language-C++\\" data-ext=\\"C++\\" data-title=\\"C++\\"><pre class=\\"language-C++\\"><code>西南科技大学 · 计算机科学与技术硕士\\n\\n12/2019 - 03/2020 autowise.ai\\n测试与运维组 · 测试\\n\\n04/2023 - 08/2023 XFUSION\\n操作系统开发部 · 操作系统研发\\n\\n03/2024 - Present 四川信息职业技术学院\\n网络与通信学院 · 助教\\n</code></pre></div>","autoDesc":true}');export{p as comp,u as data};
